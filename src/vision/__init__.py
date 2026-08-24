fa369652a8ea66cd10776c8a706af4655536eae6
Hayden Ng <81052785+haydenng12@users.noreply.github.com>
2026-08-24 16:36:29 -0400

    Export guided capture APIs

--- src/vision/__init__.py
+++ src/vision/__init__.py
@@ -1,18 +1,31 @@
 """Vision package: guided camera scanning pipeline."""
 
+from .alignment import AlignmentConfig, FaceAlignment, score_face_alignment
 from .face_geometry import FaceGeometry, FaceGeometryConfig, analyze_face_geometry, analyze_face_group
+from .guide_layout import FaceGuide, GuideProfile, build_face_guides
 from .scan_validation import FaceScan, validate_cube_state, validate_face_stickers, validate_partial_scan
 from .types import FACE_ORDER, VALID_FACE_SET, ValidationResult
+from .warp_sampling import SampledFace, SamplingConfig, StickerSample, sample_guided_face
 
 __all__ = [
     "FACE_ORDER",
+    "AlignmentConfig",
+    "FaceAlignment",
     "VALID_FACE_SET",
     "FaceGeometry",
     "FaceGeometryConfig",
+    "FaceGuide",
+    "GuideProfile",
     "FaceScan",
     "ValidationResult",
+    "SampledFace",
+    "SamplingConfig",
+    "StickerSample",
     "analyze_face_geometry",
     "analyze_face_group",
+    "build_face_guides",
+    "sample_guided_face",
+    "score_face_alignment",
     "validate_cube_state",
     "validate_face_stickers",
     "validate_partial_scan",
